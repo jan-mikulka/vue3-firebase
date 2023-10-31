45a4195a8cb936e48e050fa4363aece052b0fff0
Jan Mikulka <jan.mikulka@email.cz>
2023-10-31 12:52:32 +0100

    Add Nullable type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
+export type Nullable<T> = {
+  [P in keyof T]: T[P] | null;
+};
+
 export type MonthNumbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export type FlowerColors = 'white' | 'yellow' | 'violet' | 'brown';
